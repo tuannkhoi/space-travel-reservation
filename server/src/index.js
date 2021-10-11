6795e7212a68ede3b879e1811d1a3a51bfaf978e
Khoi Tuan <khoituan1412@gmail.com>
2021-10-11 14:11:42 +1000

    add schema

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,13 @@
 require('dotenv').config();
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+
+// pass the schema to ApolloServer
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(() => {
+	console.log(`
+		Server is running on port 4000
+		https://studio.apollographql.com/sandbox
+	`);
+});
---
+++ server/src/schema.js
@@ -0,0 +1,50 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+	type Query { #allow clients to fetch data
+		launches: [Launch]! #return an array of upcoming launches
+		launch(id: ID!): Launch #return a single Launch that matches the id
+		me: User #return details of the currently logged in User
+	}
+	type Mutation { # allow clients to modify data
+		bookTrips(launchIds: [ID]!): TripUpdateResponse!
+		cancelTrips(launchIds: ID!): TripUpdateResponse!
+		login(email: String): User
+
+	}
+	type TripUpdateResponse {
+		success: Boolean!
+		message: String
+		launches: [Launch] #good practice for a mutation to return whatever objects it modifies so the requesting client can update its cache and UI without needing to make a follow up query
+	}
+	# Each object type define should represent an object that an application client might need to interact with
+	# App will need to fetch a list of upcoming rocket launches -> define a Launch type to support that behaviour
+	type Launch {
+		id: ID!
+		site: String
+		mission: Mission # Mission and Rocket refer to other object types
+		rocket: Rocket # have to define them below
+		isBooked: Boolean!
+	}
+	type Rocket {
+		id: ID!
+		name: String
+		type: String
+	}
+	type User {
+		id: ID!
+		email: String!
+		trips: [Launch]! #array! -> cannot be null, but can be empty
+		token: String
+	}
+	type Mission {
+		name: String
+		missionPatch(size: PatchSize): String
+	}
+	enum PatchSize {
+		SMALL
+		LARGE
+	}
+`;
+
+module.exports = typeDefs;
